617ecf8cd5e1f5856ee9a6a8e12aa56e891c808a
Laurent NAVARRO <ln@altidev.com>
2021-02-03 17:10:52 +0100

    Isolation requete EcoTaxa dans ecotaxainterface.py

--- py/appli/part/ecotaxainterface.py
+++ py/appli/part/ecotaxainterface.py
@@ -22,3 +22,40 @@ def GetObjectsForTaxoHistoCompute(prj: partdatabase.part_projects, eco_sampleid:
                     and {areacol} is not NULL and classif_qual='V'
                     """.format(sampleid=eco_sampleid, areacol=areacol), None, False, psycopg2.extras.RealDictCursor)
     return lst_taxo_det
+
+
+def GetObjectsForRawExport(psampleid: int,
+                           excludenotliving: bool,
+                           includenotvalidated: bool) -> []:
+    sql = """select of.*
+        ,t0.display_name as "name", classif_qual ,ps.psampleid                        
+        ,((oh.depth_min+oh.depth_max)/2) as depth_including_offset,objid
+        ,concat(t14.name||'>',t13.name||'>',t12.name||'>',t11.name||'>',t10.name||'>',t9.name||'>'
+                ,t8.name||'>',t7.name||'>',t6.name||'>',t5.name||'>',t4.name||'>',t3.name||'>'
+                ,t2.name||'>',t1.name||'>',t0.name) taxo_hierarchy
+      from part_samples ps
+      join obj_head oh on ps.sampleid=oh.sampleid 
+      join obj_field of on of.objfid=oh.objid                      
+        join taxonomy t0 on oh.classif_id=t0.id
+        left join taxonomy t1 on t0.parent_id=t1.id
+        left join taxonomy t2 on t1.parent_id=t2.id
+        left join taxonomy t3 on t2.parent_id=t3.id
+        left join taxonomy t4 on t3.parent_id=t4.id
+        left join taxonomy t5 on t4.parent_id=t5.id
+        left join taxonomy t6 on t5.parent_id=t6.id
+        left join taxonomy t7 on t6.parent_id=t7.id
+        left join taxonomy t8 on t7.parent_id=t8.id
+        left join taxonomy t9 on t8.parent_id=t9.id
+        left join taxonomy t10 on t9.parent_id=t10.id
+        left join taxonomy t11 on t10.parent_id=t11.id
+        left join taxonomy t12 on t11.parent_id=t12.id
+        left join taxonomy t13 on t12.parent_id=t13.id
+        left join taxonomy t14 on t13.parent_id=t14.id                                           
+      where ps.psampleid=%s """
+    if excludenotliving:
+        sql += """ and coalesce(t14.name,t13.name,t12.name,t11.name,t10.name,t9.name,t8.name,t7.name
+                        ,t6.name,t5.name,t4.name,t3.name,t2.name,t1.name,t0.name)!='not-living' """
+    if includenotvalidated == False:
+        sql += " and oh.classif_qual='V' "
+    res = database.GetAll(sql, (psampleid,),cursor_factory=psycopg2.extras.RealDictCursor)
+    return res
--- py/appli/tasks/taskpartexport.py
+++ py/appli/tasks/taskpartexport.py
@@ -15,6 +15,7 @@ from appli.part import PartDetClassLimit, PartRedClassLimit, GetClassLimitTxt, C
     GetPartClassLimitListText
 from appli.part.common_sample_import import GetPathForRawHistoFile
 from appli.part.drawchart import GetTaxoHistoWaterVolumeSQLExpr
+from appli.part.ecotaxainterface import GetObjectsForRawExport
 from appli.part.part_main import GetFilteredSamples, PartstatsampleGetData
 from appli.tasks.taskmanager import AsyncTask
 
@@ -845,38 +846,11 @@ order by tree""".format(lstcatwhere)
                 zoo_file_par_p_sample_id[S["psampleid"]] = nomfichier
                 fichier = os.path.join(self.GetWorkingDir(), nomfichier)
                 with open(fichier, "wt") as f:
-                    # excludenotliving
-                    sql = """select of.*
-                        ,t0.display_name as "name", classif_qual ,ps.psampleid                        
-                        ,((oh.depth_min+oh.depth_max)/2)+{DepthOffset} as depth_including_offset,objid
-                        ,concat(t14.name||'>',t13.name||'>',t12.name||'>',t11.name||'>',t10.name||'>',t9.name||'>'
-                                ,t8.name||'>',t7.name||'>',t6.name||'>',t5.name||'>',t4.name||'>',t3.name||'>'
-                                ,t2.name||'>',t1.name||'>',t0.name) taxo_hierarchy
-                      from part_samples ps
-                      join obj_head oh on ps.sampleid=oh.sampleid 
-                      join obj_field of on of.objfid=oh.objid                      
-                        join taxonomy t0 on oh.classif_id=t0.id
-                        left join taxonomy t1 on t0.parent_id=t1.id
-                        left join taxonomy t2 on t1.parent_id=t2.id
-                        left join taxonomy t3 on t2.parent_id=t3.id
-                        left join taxonomy t4 on t3.parent_id=t4.id
-                        left join taxonomy t5 on t4.parent_id=t5.id
-                        left join taxonomy t6 on t5.parent_id=t6.id
-                        left join taxonomy t7 on t6.parent_id=t7.id
-                        left join taxonomy t8 on t7.parent_id=t8.id
-                        left join taxonomy t9 on t8.parent_id=t9.id
-                        left join taxonomy t10 on t9.parent_id=t10.id
-                        left join taxonomy t11 on t10.parent_id=t11.id
-                        left join taxonomy t12 on t11.parent_id=t12.id
-                        left join taxonomy t13 on t12.parent_id=t13.id
-                        left join taxonomy t14 on t13.parent_id=t14.id                                           
-                      where ps.psampleid=%s """.format(DepthOffset=depth_offset)
-                    if self.param.excludenotliving:
-                        sql += """ and coalesce(t14.name,t13.name,t12.name,t11.name,t10.name,t9.name,t8.name,t7.name
-                                        ,t6.name,t5.name,t4.name,t3.name,t2.name,t1.name,t0.name)!='not-living' """
-                    if self.param.includenotvalidated == False:
-                        sql += " and oh.classif_qual='V' "
-                    res = GetAll(sql, (S['psampleid'],))
+                    res = GetObjectsForRawExport(S['psampleid'],self.param.excludenotliving,self.param.includenotvalidated)
+                    if depth_offset:
+                        for row in res:
+                            if row['depth_including_offset']:
+                                row['depth_including_offset']+=depth_offset
                     cols = ['orig_id', 'objid', 'name', 'taxo_hierarchy', 'classif_qual', 'depth_including_offset',
                             'psampleid']
                     extracols = list(taxo_reverse_mapping.keys())
